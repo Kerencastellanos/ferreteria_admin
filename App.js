1fecbe44d692843e7d85803733fff78351db5a37
robert-virtual <robertocastill63@gmail.com>
2022-03-22 14:05:53 -0600

    paper

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./context";
 import { StackMenu } from "./routes";
-
+import { Provider as PaperProvider } from "react-native-paper";
 export default function App() {
   return (
-    <NavigationContainer>
-      <AuthProvider>
-        <StackMenu />
-      </AuthProvider>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <AuthProvider>
+          <StackMenu />
+        </AuthProvider>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- pages/CrearProducto.jsx
+++ pages/CrearProducto.jsx
@@ -1,9 +1,30 @@
-import { Text, StyleSheet, View } from "react-native";
+import { TouchableOpacity, StyleSheet, View } from "react-native";
+import { Divider, Menu } from "react-native-paper";
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
 
 export function CrearProducto() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  function alternarMenu() {
+    setMenuVisible(!menuVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>CrearProducto</Text>
+      <Menu
+        visible={menuVisible}
+        onDismiss={alternarMenu}
+        anchor={
+          <TouchableOpacity onPress={alternarMenu}>
+            <FontAwesome name="photo" size={100} color="black" />
+          </TouchableOpacity>
+        }
+      >
+        <Menu.Item title="Tomar Foto" icon={"camera"} />
+        <Divider />
+        <Menu.Item title="Buscar en Galeria " icon={"image"} />
+      </Menu>
     </View>
   );
 }
