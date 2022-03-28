11e647d9be2d90c3fb619e7d9f044e4a3ceb772a
robert-virtual <robertocastill63@gmail.com>
2022-03-28 13:35:07 -0600

    crear producto

--- pages/CrearProducto.jsx
+++ pages/CrearProducto.jsx
@@ -1,15 +1,48 @@
-import { TouchableOpacity, StyleSheet, View } from "react-native";
+import {
+  ActivityIndicator,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+} from "react-native";
 import { Divider, Menu } from "react-native-paper";
 import { FontAwesome } from "@expo/vector-icons";
 import { useState } from "react";
-
+import {
+  requestCameraPermissionsAsync,
+  launchCameraAsync,
+  launchImageLibraryAsync,
+  getCameraPermissionsAsync,
+} from "expo-image-picker";
 export function CrearProducto() {
   const [menuVisible, setMenuVisible] = useState(false);
-
+  const [tienePermiso, setTienePermiso] = useState(false);
+  const [cargando, setCargando] = useState(false);
   function alternarMenu() {
     setMenuVisible(!menuVisible);
   }
+  useEffect(() => {
+    solicitarPermisos();
+  }, []);
+  async function solicitarPermisos() {
+    setCargando(true);
+    const cameraPermission = await getCameraPermissionsAsync();
+    if (!cameraPermission.granted) {
+      const res = await requestCameraPermissionsAsync();
+      setCargando(false);
+      setTienePermiso(res.granted);
+    }
+  }
+  if (cargando) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={"large"} color="blue" />
+      </View>
+    );
+  }
 
+  if (cargando) {
+    return <View style={styles.container}></View>;
+  }
   return (
     <View style={styles.container}>
       <Menu
--- pages/ResumenVentas.jsx
+++ pages/ResumenVentas.jsx
@@ -1,38 +1,36 @@
 import axios from "axios";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 
 export function ResumenVentas() {
-    useEffect(() => {
-      obtenerVentas()
-      
-    }, [])
+  useEffect(() => {
+    obtenerVentas();
+  }, []);
 
-    const [ventas, setVentas] = useState([])
-    async function obtenerVentas() {
-        const {data} = await axios.get('/ventas/listar')
-        setVentas(data.ventas)
-    }
-    
-    return (
-        <View style={ styles.container} >
-            <FlatList 
-            data = {ventas}
-            keyExtractor = {(item)=>item.id}
-            renderItem = {()=>(
-                <View>
-                    <Text>Ventas</Text>
-                </View>
-            )}
-            />
-        </View>
-    )
-    
+  const [ventas, setVentas] = useState([]);
+  async function obtenerVentas() {
+    const { data } = await axios.get("/ventas/listar");
+    setVentas(data.ventas);
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={ventas}
+        keyExtractor={(item) => item.id}
+        renderItem={() => (
+          <View>
+            <Text>Ventas</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
